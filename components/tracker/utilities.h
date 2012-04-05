6c99596d1dd39ed036a46395dcd70aa05c0428fa
Yaming Liu <yuuumy@gmail.com>
2012-04-05 23:03:34 +0800

    clean up this branch for new code base

--- components/tracker/utilities.h
+++
@@ -1,4 +0,0 @@
-
-void SendKey(WORD key);
-BOOL AnsiToUnicode16(const CHAR *in_Src, WCHAR *out_Dst, INT in_MaxLen);
-BOOL AnsiToUnicode16L(const CHAR *in_Src, INT in_SrcLen, WCHAR *out_Dst, INT in_MaxLen);
--- platform/win32/build/msbuild_x86.bat
+++
@@ -1,5 +0,0 @@
-@call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
-
-@cd /D .
-
-@title Retina Build Window
