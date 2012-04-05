b178e470abbbc5421df28f79dca1b98d7a15bec9
Yaming Liu <yuuumy@gmail.com>
2012-04-06 01:20:21 +0800

    using .net/c# platform

---
+++ utility/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Com.Imola.Retina.Utility
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            try
+            {
+                Application.Run(new MainForm());
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "UserTracker", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ utility/WinForm/MainForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Com.Imola.Retina.Utility
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CanvasPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // CanvasPanel
+            // 
+            this.CanvasPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CanvasPanel.Location = new System.Drawing.Point(0, 0);
+            this.CanvasPanel.Name = "CanvasPanel";
+            this.CanvasPanel.Size = new System.Drawing.Size(602, 439);
+            this.CanvasPanel.TabIndex = 0;
+            this.CanvasPanel.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 439);
+            this.Controls.Add(this.CanvasPanel);
+            this.Name = "MainForm";
+            this.Text = "Utility";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel CanvasPanel;
+    }
+}
+
